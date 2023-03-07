99801e54a2daf45b91175138cdc6caef3f23fa1b
sanderdsan <127244715+sanderdsan@users.noreply.github.com>
2023-03-08 01:22:15 +0200

    Add files via upload

---
+++ s1l1_3/s1l1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {       
+        string[] strings = { "kpi", "refrigerators", "molusk", "1804", "SacrificE" };
+
+        var sortedChars = strings.Select(s => s.Length % 2 == 0 ? s.Last() : s.First()).OrderByDescending(c => (char)c);
+
+        Console.WriteLine(string.Join(", ", sortedChars));
+    }
+}
